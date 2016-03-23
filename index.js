52e8bf0b9a7a4e17eb83e15ec87c1588a4af973f
garyyochum <yochum.gary@gmail.com>
2016-03-23 02:09:41 -0400

    added crud methods for the demo

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var api = express();
+var bp = require('body-parser');
 var jwt = require('jsonwebtoken');
 var eJwt = require('express-jwt');
 var repo = require('./repository');
 var secret = 'garyzsuperbigsecret';
 
 //middleware
+api.use(bp.json());
 
 //routes
 api.get('/api/music', repo.get);
-
+api.get('/api/music/:id', repo.findOne);
+api.post('/api/music', repo.save);
+api.put('/api/music/:id', repo.update);
+api.delete('/api/music/:id', repo.delete);
 
 //error handler
 
--- repository.js
+++ repository.js
@@ -1,15 +1,84 @@
+var reader = require('jsonfile');
+
 exports.get = function(request, response){
-    var result = {
-        artist: 'fred v & grafix'
-    };
-    
-    response.json(result);
+    reader.readFile('./data/music.json', function(error, tunes){
+        if(error)
+            console.log(error);
+            
+        response.json(tunes);    
+    });    
 }
 
 exports.findOne = function(request, response){
+    var id = request.params.id;
     
+    reader.readFile('./data/music.json', function(error, tunes){
+        var tune = tunes.filter(function(t, index){
+           return t.id == id; 
+        });
+            
+        response.json(tune);    
+    }); 
 }
 
-exports.post = function(request, response){
+exports.save = function(request, response){
+    var tune = request.body;
+    var tunes = reader.readFileSync('./data/music.json');
     
+    if(tunes){
+        tunes.push(tune);
+        reader.writeFile('./data/music.json', tunes, function(error){
+            if(error)
+                console.log(error);
+                
+            response.json(tunes);
+        });
+    }            
+}
+
+exports.update = function(request, response){
+    var id = request.params.id;
+    var tune = request.body;
+    var tunes = reader.readFileSync('./data/music.json');
+    
+    tunes.forEach(function(t, index){
+        if(t.id == id){
+            t.artist = tune.artist;
+            t.tune = tune.tune;
+            t.album = tune.album;
+            t.year = tune.year;
+        }
+    });
+    
+    reader.writeFile('./data/music.json', tunes, function(error){
+        if(error)
+            console.log(error);
+            
+        response.json(tunes);
+    });
+}
+
+exports.delete = function(request, response){
+    var id = request.params.id;
+    var tunes = reader.readFileSync('./data/music.json');
+    
+    var index = -1;
+    tunes.forEach(function(tune, idx){
+       if(tune.id == id)
+        index = idx;           
+    });
+    
+    if(index > -1){
+        tunes.splice(index, 1);
+        
+        reader.writeFile('./data/music.json', tunes, function(error){
+            if(error)
+                console.log(error);
+                
+            response.json(tunes);
+        });
+    }
+    else{
+        response.send('could not find the tune you are trying to delete');    
+    }    
 }
